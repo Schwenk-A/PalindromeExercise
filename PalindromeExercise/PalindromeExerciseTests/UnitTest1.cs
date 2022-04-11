f9249457caa71a458498db60b4d021229fcf5e66
Andrea Schwenk <andreaschwenk3@gmail.com>
2022-04-11 12:05:31 -0500

    Updated project with correct testing

--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,27 @@
 using System;
+using PalindromeExercise;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("anna", true)]  
+        [InlineData("civic", true)]  
+        [InlineData("level", true)]  
+        [InlineData("madam", true)]  
+        [InlineData("mark", false)]  
+        [InlineData("mistletoe", false)]  
+          
+        public void PalindromeTester(string word, bool result)
         {
+            var wordSmith = new WordSmith();
 
+            var actual = wordSmith.IsAPalindrome(word);
+
+            Assert.Equal(result, actual);
         }
+        
     }
 }
